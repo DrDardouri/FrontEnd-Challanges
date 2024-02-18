f48bd4e6f09f56e9cf524f8fab8857da1dae7b92
mhamed <dr.dardourimhamed@gmail.com>
2024-02-18 21:02:53 +0300

    news homepage 1.0

--- FrontendMentor/junior/3-news-homepage/script.js
+++ FrontendMentor/junior/3-news-homepage/script.js
@@ -1,6 +1,24 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navList = document.querySelector(".links");
+"use-strict";
 
-navToggle.addEventListener("click", function () {
-    navList.classList.toggle("show-links");
+const openNavBtn = document.querySelector(".open-nav-btn");
+const closeNavBtn = document.querySelector(".close-nav-btn");
+const bodyEl = document.querySelector(".body");
+
+// event handlers
+
+// open mobile navigation
+openNavBtn.addEventListener("click", function () {
+    bodyEl.classList.add("active-body");
+});
+
+// close mobile navigation
+closeNavBtn.addEventListener("click", function () {
+    bodyEl.classList.remove("active-body");
+});
+
+// close the mobile navigation when screen is over 800px
+window.addEventListener("resize", function () {
+    if (window.matchMedia("(min-width: 800px)").matches) {
+        bodyEl.classList.remove("active-body");
+    }
 });
